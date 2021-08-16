07c8800103f5ca836d785bec0ec19aa1dcc7bd3b
Ashutosh Mohanty <75971776+Ashutosh102@users.noreply.github.com>
2021-08-16 23:27:45 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -42,7 +42,7 @@ const Home = ({state, styles, ...actions}) => {
             columnClassName="my-masonry-grid_column">
             {(!state.search ? state.notes_list : state.search_list).filter(n=>{ return ((n.id !== state.pinned_id) && (n.id !== state.popUp_id)) }).map((item,index)=>
               <li key={index} className="list-item" >
-                <span className="span1">{item.title} <button className="pin-button" onClick={e=>actions.pinNote(item.id)}><img className="pin" src="./push-pin.png"/></button></span> 
+               <span className="span1">{item.title} <button className="pin-button" onClick={e=>actions.pinNote(item.id)}><img className="pin" src="./push-pin.png" alt="icon"/></button></span> 
                 <span className="span2">{item.input}</span> 
                 <button className="list-button" onClick={e => actions.showNote(item.id)}>Edit</button>
                 <button className="list-button" onClick={e=>actions.removeFromNotes(index, item.id)}>Delete</button></li>)}
